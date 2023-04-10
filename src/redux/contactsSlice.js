b489be9d3f8b481bec62bd715b438d59dee76cf0
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-04-10 18:54:01 +0300

    change delete reducer

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -38,10 +38,7 @@ const constacsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(
-        contact => contact.id === action.payload.id
-      );
-      state.items.splice(index, 1);
+      state.items = state.items.filter(item => item.id !== action.payload)
     },
     [deleteContact.rejected]: handleRejected,
     
